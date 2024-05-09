5780f74e262dc578aae5bf01c8b4848d05e1a0d0
omerKaduri1 <kaduriomer@gmail.com>
2024-05-09 20:03:43 +0300

    Add app header with routing

---
+++ src/cmps/AppHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="app-header">
+    <RouterLink to="/">
+      <p>MisterBIT<span>Coin</span></p>
+    </RouterLink>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/contact">Contacts</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.app-header {
+  height: 100px;
+  border-bottom: 1px solid #dfdbdb;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & p {
+    margin: 1.5em;
+    font-size: 1.4em;
+    cursor: pointer;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    margin-right: 40px;
+    column-gap: 2em;
+    font-size: 1em;
+
+    @media (max-width: 600px) {
+      column-gap: 1em;
+    }
+
+    & a {
+      cursor: pointer;
+      text-decoration: none;
+      color: inherit;
+
+      &.router-link-exact-active {
+        font-weight: 700;
+        font-size: 1.05em;
+      }
+    }
+  }
+
+  & span {
+    color: rgb(18, 122, 18);
+  }
+}
+</style>
