d4d4c413a2936d1e7ee1a5f44e06b8f2868390f1
omerKaduri1 <kaduriomer@gmail.com>
2024-05-15 16:10:22 +0300

    Fix transaction list layout

--- src/cmps/TransactionList.vue
+++ src/cmps/TransactionList.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="transactions-list" v-if="transactions.length">
     <h4>{{ title }}</h4>
-    <ul v-for="transaction in transactions" :key="transaction.at">
-      <li>
+    <ul>
+      <li v-for="transaction in transactions" :key="transaction.at">
         <TransactionPreview :transaction="transaction" />
       </li>
     </ul>
@@ -28,17 +28,25 @@ export default {
 
 <style lang="scss">
 .transactions-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ul {
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 0;
+    gap: 1em;
 
-    li{
+    li {
       list-style: none;
     }
+  }
 
-    h4 {
-        font-size: 1.1em;
-        font-weight: 500;
-    }
+  h4 {
+    font-size: 1.1em;
+    font-weight: 500;
+  }
 }
 </style>
